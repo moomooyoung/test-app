7f146347b0527beb6af3562b7651cedc70076d5d
kimmooyoung <mykim@tdgl.co.kr>
2025-09-01 16:35:51 +0900

    chore: demo trigger - update app.js

--- app.js
+++ app.js
@@ -2,5 +2,5 @@ const express = require('express');
 const app = express();
 const PORT = 3000;
 
-app.get('/', (req, res) => res.send('Hello World!'));
+app.get('/', (req, res) => res.send('Hellow TDG!'));
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
