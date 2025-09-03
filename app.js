6f5625811b2b7f6dcd1f1f6903cf7612db6d787d
kimmooyoung <mykim@tdgl.co.kr>
2025-09-03 13:22:05 +0900

    데모

--- app.js
+++ app.js
@@ -2,5 +2,5 @@ const express = require('express');
 const app = express();
 const PORT = 3000;
 
-app.get('/', (req, res) => res.send('Hellow moo!'));
+app.get('/', (req, res) => res.send('Hellow mo!'));
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
