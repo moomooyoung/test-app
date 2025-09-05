3263269dcd2d3c0e30ea51325ff84964bb4d1b73
kimmooyoung <mykim@tdgl.co.kr>
2025-09-05 10:18:40 +0900

    Update app.js message and Helm chart deployment

--- app.js
+++ app.js
@@ -2,5 +2,5 @@ const express = require('express');
 const app = express();
 const PORT = 3000;
 
-app.get('/', (req, res) => res.send('Hellow mo!'));
+app.get('/', (req, res) => res.send('Hellow m!'));
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
